c544f3fea880813b0f6bf4e6260b919aca8ca495
Ahmed Sheimy <ahmedemad456478@gmail.com>
2023-07-25 19:36:43 -0700

    Ahmed Sheimy

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,21 +9,21 @@
  */
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **current)
 {
-    listint_t *tmp = (*current)->next;
+	listint_t *tmp = (*current)->next;
 
-    if ((*current)->prev != NULL)
-        (*current)->prev->next = tmp;
-    else
-        *list = tmp;
-    tmp->prev = (*current)->prev;
-    (*current)->next = tmp->next;
-    if (tmp->next != NULL)
-        tmp->next->prev = *current;
-    else
-        *tail = *current;
-    (*current)->prev = tmp;
-    tmp->next = *current;
-    *current = tmp;
+	if ((*current)->prev != NULL)
+		(*current)->prev->next = tmp;
+	else
+		*list = tmp;
+	tmp->prev = (*current)->prev;
+	(*current)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *current;
+	else
+		*tail = *current;
+	(*current)->prev = tmp;
+	tmp->next = *current;
+	*current = tmp;
 }
 
 /**
@@ -35,21 +35,21 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **current)
  */
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **current)
 {
-    listint_t *tmp = (*current)->prev;
+	listint_t *tmp = (*current)->prev;
 
-    if ((*current)->next != NULL)
-        (*current)->next->prev = tmp;
-    else
-        *tail = tmp;
-    tmp->next = (*current)->next;
-    (*current)->prev = tmp->prev;
-    if (tmp->prev != NULL)
-        tmp->prev->next = *current;
-    else
-        *list = *current;
-    (*current)->next = tmp;
-    tmp->prev = *current;
-    *current = tmp;
+	if ((*current)->next != NULL)
+		(*current)->next->prev = tmp;
+	else
+		*tail = tmp;
+	tmp->next = (*current)->next;
+	(*current)->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *current;
+	else
+		*list = *current;
+	(*current)->next = tmp;
+	tmp->prev = *current;
+	*current = tmp;
 }
 
 /**
@@ -59,35 +59,35 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **current)
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *tail, *current;
-    bool shaken_not_stirred = false;
+	listint_t *tail, *current;
+	bool shaken_not_stirred = false;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    for (tail = *list; tail->next != NULL;)
-        tail = tail->next;
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
 
-    while (shaken_not_stirred == false)
-    {
-        shaken_not_stirred = true;
-        for (current = *list; current != tail; current = current->next)
-        {
-            if (current->n > current->next->n)
-            {
-                swap_node_ahead(list, &tail, &current);
-                print_list((const listint_t *)*list);
-                shaken_not_stirred = false;
-            }
-        }
-        for (current = current->prev; current != *list; current = current->prev)
-        {
-            if (current->n < current->prev->n)
-            {
-                swap_node_behind(list, &tail, &current);
-                print_list((const listint_t *)*list);
-                shaken_not_stirred = false;
-            }
-        }
-    }
+	while (shaken_not_stirred == false)
+	{
+		shaken_not_stirred = true;
+		for (current = *list; current != tail; current = current->next)
+		{
+			if (current->n > current->next->n)
+			{
+				swap_node_ahead(list, &tail, &current);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+		for (current = current->prev; current != *list; current = current->prev)
+		{
+			if (current->n < current->prev->n)
+			{
+				swap_node_behind(list, &tail, &current);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+	}
 }
