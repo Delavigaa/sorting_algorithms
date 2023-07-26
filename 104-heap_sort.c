0ebf6514bb47ae992ed059ba5c88767816cb368f
Ahmed elqanwaty <ahemedf2312@gmail.com>
2023-07-25 19:46:19 -0700

    Ahmed Elqanwaty

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -7,11 +7,11 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -25,23 +25,23 @@ void swap_ints(int *a, int *b)
  */
 void max_heapify(int *array, size_t size, size_t base_idx, size_t root_idx)
 {
-    size_t left_idx, right_idx, largest_idx;
+	size_t left_idx, right_idx, largest_idx;
 
-    left_idx = 2 * root_idx + 1;
-    right_idx = 2 * root_idx + 2;
-    largest_idx = root_idx;
+	left_idx = 2 * root_idx + 1;
+	right_idx = 2 * root_idx + 2;
+	largest_idx = root_idx;
 
-    if (left_idx < base_idx && array[left_idx] > array[largest_idx])
-        largest_idx = left_idx;
-    if (right_idx < base_idx && array[right_idx] > array[largest_idx])
-        largest_idx = right_idx;
+	if (left_idx < base_idx && array[left_idx] > array[largest_idx])
+		largest_idx = left_idx;
+	if (right_idx < base_idx && array[right_idx] > array[largest_idx])
+		largest_idx = right_idx;
 
-    if (largest_idx != root_idx)
-    {
-        swap_ints(array + root_idx, array + largest_idx);
-        print_array(array, size);
-        max_heapify(array, size, base_idx, largest_idx);
-    }
+	if (largest_idx != root_idx)
+	{
+		swap_ints(array + root_idx, array + largest_idx);
+		print_array(array, size);
+		max_heapify(array, size, base_idx, largest_idx);
+	}
 }
 
 /**
@@ -55,18 +55,18 @@ void max_heapify(int *array, size_t size, size_t base_idx, size_t root_idx)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i;
+	int i;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = (size / 2) - 1; i >= 0; i--)
-        max_heapify(array, size, size, i);
+	for (i = (size / 2) - 1; i >= 0; i--)
+		max_heapify(array, size, size, i);
 
-    for (i = size - 1; i > 0; i--)
-    {
-        swap_ints(array, array + i);
-        print_array(array, size);
-        max_heapify(array, size, i, 0);
-    }
+	for (i = size - 1; i > 0; i--)
+	{
+		swap_ints(array, array + i);
+		print_array(array, size);
+		max_heapify(array, size, i, 0);
+	}
 }
--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -7,11 +7,11 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -24,19 +24,19 @@ void swap_ints(int *a, int *b)
  */
 void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow)
 {
-    size_t i, jump = seq / 2;
+	size_t i, jump = seq / 2;
 
-    if (seq > 1)
-    {
-        for (i = start; i < start + jump; i++)
-        {
-            if ((flow == UP && array[i] > array[i + jump]) ||
-                (flow == DOWN && array[i] < array[i + jump]))
-                swap_ints(array + i, array + i + jump);
-        }
-        bitonic_merge(array, size, start, jump, flow);
-        bitonic_merge(array, size, start + jump, jump, flow);
-    }
+	if (seq > 1)
+	{
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+				(flow == DOWN && array[i] < array[i + jump]))
+				swap_ints(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
 }
 
 /**
@@ -49,21 +49,21 @@ void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow)
  */
 void bitonic_sequence(int *array, size_t size, size_t start, size_t seq, char flow)
 {
-    size_t cut = seq / 2;
-    char *str = (flow == UP) ? "UP" : "DOWN";
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
 
-    if (seq > 1)
-    {
-        printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
-        print_array(array + start, seq);
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
 
-        bitonic_sequence(array, size, start, cut, UP);
-        bitonic_sequence(array, size, start + cut, cut, DOWN);
-        bitonic_merge(array, size, start, seq, flow);
+		bitonic_sequence(array, size, start, cut, UP);
+		bitonic_sequence(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
 
-        printf("Result [%lu/%lu] (%s):\n", seq, size, str);
-        print_array(array + start, seq);
-    }
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
 }
 
 /**
@@ -77,8 +77,8 @@ void bitonic_sequence(int *array, size_t size, size_t start, size_t seq, char fl
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    bitonic_sequence(array, size, 0, size, UP);
+	bitonic_sequence(array, size, 0, size, UP);
 }
