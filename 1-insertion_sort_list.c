be08b11a9009a082bf668582e77656aadbf4886e
Ahmed Sheimy <ahmedemad456478@gmail.com>
2023-07-25 19:24:37 -0700

    Ahmed Sheimy

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,23 +1,20 @@
 #include "sort.h"
 
 /**
- * swap_nodes - In a listint_t doubly-linked list, switch two nodes..
+ * swap_nodes - In a listint_t doubly-linked list, switch two nodes.
  * @h: A pointer to the head of the doubly-linked list.
- * @n1:a pointing device at the doubly-linked list's head.
+ * @n1: a pointing device at the doubly-linked list's head.
  * @n2: the following node to switch.
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	listint_t *tmp_next = (*n1)->next;
-	listint_t *tmp_prev = (*n1)->prev;
-
 	(*n1)->next = n2->next;
 	if (n2->next != NULL)
 		n2->next->prev = *n1;
-	n2->prev = tmp_prev;
+	n2->prev = (*n1)->prev;
 	n2->next = *n1;
-	if (tmp_prev != NULL)
-		tmp_prev->next = n2;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
 	else
 		*h = n2;
 	(*n1)->prev = n2;
